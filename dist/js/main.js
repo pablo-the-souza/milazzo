00f1acddb6d6285075ef13be96fa074b114ccd3d
Pablo Souza <pablo.the.souza@gmail.com>
2019-11-18 10:40:01 +1000

    menu-btn

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,18 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn'); //querySelector selects elements of HTML
 const menu = document.querySelector('.menu');
 const menuNav= document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item'); //querySelector only grabs the first item
 
 // Set Initial State of Menu
-let showMenu = false;
+let showMenu = false; //let because you want to reassign it at some point
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('click', toggleMenu); //listen for a click event fires toggleMenu
 
 function toggleMenu() {
     if(!showMenu) {
-        menuBtn.classList.add('close'); 
+        menuBtn.classList.add('close'); //adds a class
         menu.classList.add('show'); 
         menuNav.classList.add('show'); 
         menuBranding.classList.add('show'); 
@@ -21,7 +21,7 @@ function toggleMenu() {
 // Set Menu State
 showMenu = true; 
     } else {
-        menuBtn.classList.remove('close'); 
+        menuBtn.classList.remove('close'); //removes a class
         menu.classList.remove('show');
         menuNav.classList.remove('show'); 
         menuBranding.classList.remove('show');
